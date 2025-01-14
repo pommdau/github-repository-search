b5a6009a2f4e1b36178c54a56b2b2a3e2238c604
IKEH1024 <pondau1024@gmail.com>
2025-01-14 19:27:34 +0900

    wip

--- IKEHGitHubAPIDemo/IKEHGitHubAPIDemo/AsyncValue.swift
+++ IKEHGitHubAPIDemo/IKEHGitHubAPIDemo/AsyncValue.swift
@@ -6,9 +6,96 @@
 //
 //  Refs: [非同期処理をラクにする！SwiftUIでの設計パターンの紹介](https://zenn.dev/timetree/articles/97ad70c4611894)
 
-enum AsyncValue<Item, E: Error> {
+import SwiftUI
+
+enum AsyncValue<T, E: Error> {
     case initial /// 読み込み開始前
-    case loading(Item?) /// 読み込み中 or リフレッシュ中
-    case loaded(Item) /// 読み込み成功
-    case error(E, Item?) ///エラー
+    case loading(T?) /// 読み込み中 or リフレッシュ中
+    case loaded(T) /// 読み込み成功
+    case error(E, T?) ///エラー
+}
+
+enum AsyncValues<T, E: Error> {
+    case initial /// 読み込み開始前
+    case loading([T]) /// 読み込み中 or リフレッシュ中
+    case loaded([T]) /// 読み込み成功
+    case error(E, [T]) ///エラー
+}
+
+
+struct AsyncValueView<
+    T,
+    E: Error,
+    DataView: View,
+    InitialView: View,
+    LoadingView: View,
+    ErrorView: View
+>: View {
+    var values: AsyncValues<T, E>
+    var dataView: ([T]) -> DataView
+    var initialView: () -> InitialView
+    var loadingView: ([T]) -> LoadingView
+    var errorView: (E, [T]) -> ErrorView
+
+    var body: some View {
+        ZStack(alignment: .center) {
+            switch values {
+            case .initial:
+                initialView()
+            case let .loading(elements):
+                loadingView(elements)
+            case let .loaded(t):
+                dataView(t)
+            case let .error(e, values):
+                errorView(e, values)
+            }
+        }
+    }
+}
+
+
+extension AsyncValueView {
+//    /// dataViewだけ
+//    init(value: AsyncValue<T, E>,
+//         @ViewBuilder dataView: @escaping (T) -> DataView)
+//        where InitialView == LoadingView,
+//        LoadingView == ProgressView<EmptyView, EmptyView>,
+//    ErrorView == EmptyView {
+//        self.value = value
+//        self.dataView = dataView
+//        initialView = ProgressView()
+//        loadingView = ProgressView()
+//        errorView = { error in EmptyView() }
+//    }
+//
+//    /// dataView, loadingView
+//    /// 省略
+//    /// dataView, errorView
+//    /// 省略
+//
+//    /// dataView, loadingView, errorView
+//    init(value: AsyncValue<T, E>,
+//         @ViewBuilder dataView: @escaping (T) -> DataView,
+//         @ViewBuilder loadingView: @escaping () -> LoadingView,
+//         @ViewBuilder errorView: @escaping (E) -> ErrorView)
+//        where InitialView == LoadingView {
+//        self.value = value
+//        self.dataView = dataView
+//        initialView = loadingView()
+//        self.loadingView = loadingView()
+//        self.errorView = errorView
+//    }
+    
+//    init(values: AsyncValues<T, E>,
+//         @ViewBuilder dataView: @escaping ([T]) -> DataView,
+//         @ViewBuilder initialView: @escaping () -> InitialView,
+//         @ViewBuilder loadingView: @escaping ([T]) -> LoadingView,
+//         @ViewBuilder errorView: @escaping (E, [T]) -> ErrorView)
+//    {
+//        self.values = values
+//        self.dataView = dataView
+//        self.initialView = initialView
+//        self.loadingView = loadingView
+//        self.errorView = errorView
+//    }
 }
--- IKEHGitHubAPIDemo/IKEHGitHubAPIDemo/Views/ContentView.swift
+++ IKEHGitHubAPIDemo/IKEHGitHubAPIDemo/Views/ContentView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 import IKEHGitHubAPI
 
+struct SampleView: View {
+    var body: some View {
+        Text("sample")
+    }
+}
+
 struct ContentView: View {
     @State private var keyword = "Swift"
-    @State private var repos: [Repo] = []
+//    @State private var repos: AsyncValues<Repo, GitHubAPIError> = .initial
+//    @State private var repos: AsyncValues<Repo, GitHubAPIError> = .loaded(Repo.sampleData)
+    @State private var repos: AsyncValues<Repo, GitHubAPIError> = .loading(Array(Repo.sampleData[0..<1]))
+//    @State private var repos: [Repo] = []
     @State private var lastRepoID: Int?
     @State private var relationLink: RelationLink?
     
@@ -21,16 +30,25 @@ struct ContentView: View {
                 searchButton()
             }
             .padding()
-
-            List {
-                ForEach(repos) { repo in
+            
+            AsyncValueView(values: repos) { repos in
+                List(repos) { repo in
                     RepoCell(repo: repo)
                         .padding(.vertical, 4)
-                        .onAppear() {                            
-                            handleLoadMoreRepo()
-                        }
                 }
-//                loadMoreButton()
+            } initialView: {
+                Text("検索してください！")
+            } loadingView: { repos in
+                List {
+                    ForEach(repos) { repo in
+                        RepoCell(repo: repo)
+                            .padding(.vertical, 4)
+                    }
+                    ProgressView()
+                        .padding(.vertical, 8)
+                }
+            } errorView: { error, repos in
+                Text(error.localizedDescription)
             }
         }
     }
@@ -45,10 +63,10 @@ extension ContentView {
             self.relationLink = nil
             Task {
                 do {
-                    let response = try await GitHubAPIClient.shared.searchRepos(keyword: keyword)
-                    self.repos = response.items
-                    self.lastRepoID = repos.last?.id
-                    self.relationLink = response.relationLink
+//                    let response = try await GitHubAPIClient.shared.searchRepos(keyword: keyword)
+//                    self.repos = response.items
+//                    self.lastRepoID = repos.last?.id
+//                    self.relationLink = response.relationLink
                 } catch {
                     print(error.localizedDescription)
                 }
@@ -77,25 +95,26 @@ extension ContentView {
     
     private func handleLoadMoreRepo() {
         
-        guard
-            let nextLink = relationLink?.next,
-            let lastRepoID = repos.last?.id else {
-            return
-        }
-        
-        print(lastRepoID)
-//        return;
-        
-        Task {
-            do {
-                let response = try await GitHubAPIClient.shared.searchRepos(keyword: nextLink.keyword, page: nextLink.page)
-                self.repos.append(contentsOf: response.items)
-                self.lastRepoID = repos.last?.id
-                self.relationLink = response.relationLink
-            } catch {
-                print(error.localizedDescription)
-            }
-        }
+//        guard
+//            case let repos =
+//            let nextLink = relationLink?.next,
+//            let lastRepoID = repos.last?.id else {
+//            return
+//        }
+//        
+//        print(lastRepoID)
+////        return;
+//        
+//        Task {
+//            do {
+//                let response = try await GitHubAPIClient.shared.searchRepos(keyword: nextLink.keyword, page: nextLink.page)
+//                self.repos.append(contentsOf: response.items)
+//                self.lastRepoID = repos.last?.id
+//                self.relationLink = response.relationLink
+//            } catch {
+//                print(error.localizedDescription)
+//            }
+//        }
     }
 }
 
