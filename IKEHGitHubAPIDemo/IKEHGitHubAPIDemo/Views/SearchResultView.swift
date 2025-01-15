b74c3be20f22506fec27d336c15084534cbf2146
IKEH1024 <pondau1024@gmail.com>
2025-01-15 10:18:42 +0900

    remove some items

--- IKEHGitHubAPIDemo/IKEHGitHubAPIDemo/Views/SearchResultView.swift
+++ IKEHGitHubAPIDemo/IKEHGitHubAPIDemo/Views/SearchResultView.swift
@@ -1,131 +1,94 @@
 //
-//  ContentView.swift
+//  SearchResultView2.swift
 //  IKEHGitHubAPIDemo
 //
-//  Created by HIROKI IKEUCHI on 2025/01/01.
+//  Created by HIROKI IKEUCHI on 2025/01/15.
 //
 
 import SwiftUI
-import IKEHGitHubAPI
-
-struct SampleView: View {
-    var body: some View {
-        Text("sample")
-    }
-}
 
 struct SearchResultView: View {
-    
-    // isSearchingは.searchableと同じViewで使用できないため、本Viewを切り出している
-    @Environment(\.isSearching) private var isSearching: Bool
-    @Environment(\.dismissSearch) private var dismissSearch
-    
-    let keyword: String
-    
-    @State private var repos: AsyncValues<Repo, GitHubAPIError> = .initial
-//        @State private var repos: AsyncValues<Repo, GitHubAPIError> = .loading([])
-//    @State private var repos: AsyncValues<Repo, GitHubAPIError> = .loaded(Repo.sampleData)
-//    @State private var repos: AsyncValues<Repo, GitHubAPIError> = .loaded(Array(Repo.sampleData[0..<2]))
-//    @State private var repos: AsyncValues<Repo, GitHubAPIError> = .loading(Array(Repo.sampleData[0..<2]))
-//    @State private var repos: [Repo] = []
-    @State private var lastRepoID: Int?
-    @State private var relationLink: RelationLink?
+    let repos: [Repo]
+    let status: SearchStatus
+    var bottomCellOnAppear: (Int) -> Void = { _ in }
     
     var body: some View {
-        AsyncValuesView(values: repos) { repos in
-            List(repos) { repo in
-                RepoCell(repo: repo)
-                    .padding(.vertical, 4)
-            }
-        } initialView: {
-            Text("Search GitHub Repositories!")
-                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-        } loadingView: { repos in
-            VStack {
-                List(repos) { repo in
-                    RepoCell(repo: repo)
-                        .padding(.vertical, 4)
-                    Divider()
-                }
-                ProgressView()
+        VStack {
+            switch status {
+            case .initial:
+                Text("Search GitHub Repositories!")
                     .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
-                    .padding(.vertical, 8)
+            case .loading, .loaded, .error:
+                dataView(repos: repos, status: status)
             }
-        } errorView: { error, repos in
-            Text(error.localizedDescription)
-        }
-        .onChange(of: isSearching) { oldValue, newValue in
-            print(newValue)
+            Spacer()
         }
     }
-}
-
-extension SearchResultView {
     
     @ViewBuilder
-    private func searchButton() -> some View {
-        Button("Search") {
-            self.repos = .loading([])
-            self.lastRepoID = nil
-            self.relationLink = nil
-            Task {
-                do {
-                    let response = try await GitHubAPIClient.shared.searchRepos(keyword: keyword)
-                    let repos = response.items
-                    self.repos = .loaded(repos)
-                    self.lastRepoID = repos.last?.id
-                    self.relationLink = response.relationLink
-                } catch {
-                    print(error.localizedDescription)
+    private func dataView(repos: [Repo], status: SearchStatus) -> some View {
+        if status != .loading && repos.isEmpty {
+            Text("No Result")
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+        } else {
+            List {
+                ForEach(repos) { repo in
+                    RepoCell(repo: repo)
+                        .padding(.vertical, 4)
+                        .onAppear {
+                            guard let lastRepo = repos.last else {
+                                return
+                            }
+                            if lastRepo.id == repo.id {
+                                bottomCellOnAppear(repo.id)
+                            }
+                        }
+                }
+                if status == .loading {
+                    ProgressView("searching...")
+                        .listRowBackground(Color(uiColor: UIColor.systemGroupedBackground))
+                        .frame(maxWidth: .infinity, alignment: .center)
                 }
             }
         }
-        .disabled(keyword.isEmpty)
+    }
+    
+    @ViewBuilder
+    private func loadingView(repos: [Repo]) -> some View {
+        List {
+            Section {
+                ForEach(repos) { repo in
+                    RepoCell(repo: repo)
+                        .padding(.vertical, 4)
+                }
+            }
+            ProgressView()
+                .listRowBackground(Color(uiColor: UIColor.systemGroupedBackground))
+                .frame(maxWidth: .infinity, alignment: .center)
+        }
     }
     
 //    @ViewBuilder
-//    private func loadMoreButton() -> some View {
-//        if let nextLink = relationLink?.next {
-//            Button("Load More...") {
-//                Task {
-//                    do {
-//                        let response = try await GitHubAPIClient.shared.searchRepos(keyword: nextLink.keyword, page: nextLink.page)
-//                        self.repos.append(contentsOf: response.items)
-//                        self.lastRepoID = repos.last?.id
-//                        self.relationLink = response.relationLink
-//                    } catch {
-//                        print(error.localizedDescription)
-//                    }
-//                }
-//            }
+//    private func progressView() -> some View {
+//        switch asyncRepos {
+//        case .loading(_):
+//            ProgressView()
+//        default:
+//            EmptyView()
 //        }
 //    }
-    
-    private func handleLoadMoreRepo() {
-        
-//        guard
-//            case let repos =
-//            let nextLink = relationLink?.next,
-//            let lastRepoID = repos.last?.id else {
-//            return
-//        }
-//        
-//        print(lastRepoID)
-////        return;
-//        
-//        Task {
-//            do {
-//                let response = try await GitHubAPIClient.shared.searchRepos(keyword: nextLink.keyword, page: nextLink.page)
-//                self.repos.append(contentsOf: response.items)
-//                self.lastRepoID = repos.last?.id
-//                self.relationLink = response.relationLink
-//            } catch {
-//                print(error.localizedDescription)
-//            }
-//        }
-    }
 }
 
-#Preview {
-    SearchResultView(keyword: "Swift")
+#Preview("SearchResultView2") {
+    SearchResultView(repos: Array(Repo.sampleData[0...6]),
+                      status: .loaded)
+}
+
+#Preview("SearchResultView2") {
+    NavigationStack {
+        SearchResultView(repos: Array(Repo.sampleData[0...1]),
+                          status: .loading)
+        
+    }
+    .searchable(text: .constant("hoge"), prompt: "Enter Keyword")
 }
---
+++ IKEHGitHubAPIDemo/IKEHGitHubAPIDemo/Views/SearchScreenViewState.swift
@@ -0,0 +1,71 @@
+//
+//  SearchScreenViewState.swift
+//  IKEHGitHubAPIDemo
+//
+//  Created by HIROKI IKEUCHI on 2025/01/15.
+//
+
+import Foundation
+
+enum SearchStatus: Equatable {
+    case initial /// 読み込み開始前
+    case loading /// 読み込み中 or リフレッシュ中
+    case loaded /// 読み込み成功
+    case error(Error) ///エラー
+
+    static func ==(lhs: SearchStatus, rhs: SearchStatus) -> Bool {
+        switch (lhs, rhs) {
+        case (.initial, .initial),
+             (.loading, .loading),
+             (.loaded, .loaded):
+            return true
+        case let (.error(lhsError), .error(rhsError)):
+            return lhsError.localizedDescription == rhsError.localizedDescription
+        default:
+            return false
+        }
+    }
+}
+
+@MainActor
+@Observable
+final class SearchScreenViewState {
+    var keyword: String = "Swift"
+    var repos: [Repo] = []
+    var searchStatus: SearchStatus = .initial
+    var relationLink: RelationLink?
+    
+    func handleSearchKeyword() {
+        self.repos = []
+        self.searchStatus = .loading
+        self.relationLink = nil
+        Task {
+            do {
+                let response = try await GitHubAPIClient.shared.searchRepos(keyword: keyword)
+                self.repos = response.items
+                self.searchStatus = .loaded
+                self.relationLink = response.relationLink
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func handleSearchMore() {
+        guard let nextLink = relationLink?.next else {
+            return
+        }
+        searchStatus = .loading
+        Task {
+            do {
+                let response = try await GitHubAPIClient.shared.searchRepos(keyword: nextLink.keyword, page: nextLink.page)
+                repos.append(contentsOf: response.items)
+                searchStatus = .loaded
+                relationLink = response.relationLink
+            } catch {
+                print(error.localizedDescription)
+                searchStatus = .error(error)
+            }
+        }
+    }
+}
