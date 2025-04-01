708a69e932e7ac03164879553734fec63daf756f
IKEH1024 <pondau1024@gmail.com>
2025-04-01 16:14:22 +0900

    wip

--- IKEHGitHubAPIDemo/IKEHGitHubAPIDemo/GitHubAPI/DataLayer/Store/TokenStore.swift
+++ IKEHGitHubAPIDemo/IKEHGitHubAPIDemo/GitHubAPI/DataLayer/Store/TokenStore.swift
@@ -9,10 +9,17 @@ import SwiftUI
 import KeychainAccess
 
 protocol TokenStoreProtocol: Actor {
+    
+    // MARK: - Property
+    
     var accessToken: String? { get set }
     var accessTokenExpiresAt: Date? { get set }
     var isAccessTokenValid: Bool { get }
     @MainActor var lastLoginStateID: String { get set }
+    var userDefaults: UserDefaults { get }
+    var keychain: Keychain { get }
+    
+    // MARK: - Methods
     
     func updateTokens(accessToken: String?, accessTokenExpiresAt: Date?)
     @MainActor
@@ -20,64 +27,10 @@ protocol TokenStoreProtocol: Actor {
     func deleteAll()
 }
 
-final actor TokenStore: TokenStoreProtocol {
-                
-    /// シングルトン用インスタンス
-    static let shared: TokenStore = .init()
-        
-    // MARK: - Property(Public)
     
-    /// アクセストークン
-    var accessToken: String? {
-        didSet {
-            keychain[Keychain.Constant.Key.accessToken] = accessToken
-        }
-    }
-    
-    /// アクセストークンの有効期限(1年間)
-    var accessTokenExpiresAt: Date? {
-        didSet {
-            userDefaults.set(accessTokenExpiresAt, forKey: "TokenStore.accessTokenExpiresAt")
-        }
-    }
-    
-    /// 最後のログインセッションID
-    @MainActor var lastLoginStateID = ""
-    
-    // MARK: - Property(Private)
-    
-    private let keychain: Keychain
-    private let userDefaults: UserDefaults
-     
-    /// 有効なアクセストークンがあるかどうか
-    var isAccessTokenValid: Bool {
-        if accessToken != nil,
-           let accessTokenExpiresAt {
-            return accessTokenExpiresAt.compare(.now) == .orderedDescending // トークンが有効期限内か
-        }
-        
-        return false
-    }
-    
-    // MARK: - LifeCycle
-            
-    init(
-        keyChain: Keychain = Keychain(service: Keychain.Constant.Service.oauth),
-        userDefaults: UserDefaults = .standard
-    ) {
-        // DI
-        self.keychain = keyChain
-        self.userDefaults = userDefaults
-        
-        // 保存値の復元
-        self.accessToken = self.keychain[Keychain.Constant.Key.accessToken]
-        self.accessTokenExpiresAt = self.userDefaults.value(forKey: "TokenStore.accessTokenExpiresAt") as? Date
-    }
-}
-
 // MARK: - CRUD
 
-extension TokenStore {
+extension TokenStoreProtocol {
     
     // MARK: Create/Update
     
@@ -105,3 +58,60 @@ extension TokenStore {
         accessTokenExpiresAt = nil
     }
 }
+
+
+final actor TokenStore: TokenStoreProtocol {
+                
+    /// シングルトン用インスタンス
+    static let shared: TokenStore = .init()
+        
+    // MARK: - Property(Public)
+    
+    /// アクセストークン
+    var accessToken: String? {
+        didSet {
+            keychain[Keychain.Constant.Key.accessToken] = accessToken
+        }
+    }
+    
+    /// アクセストークンの有効期限(1年間)
+    var accessTokenExpiresAt: Date? {
+        didSet {
+            userDefaults.set(accessTokenExpiresAt, forKey: "TokenStore.accessTokenExpiresAt")
+        }
+    }
+    
+    /// 最後のログインセッションID
+    @MainActor var lastLoginStateID = ""
+    
+    // MARK: - Property(Private)
+    
+    let keychain: Keychain
+    let userDefaults: UserDefaults
+     
+    /// 有効なアクセストークンがあるかどうか
+    var isAccessTokenValid: Bool {
+        if accessToken != nil,
+           let accessTokenExpiresAt {
+            return accessTokenExpiresAt.compare(.now) == .orderedDescending // トークンが有効期限内か
+        }
+        
+        return false
+    }
+    
+    // MARK: - LifeCycle
+            
+    init(
+        keyChain: Keychain = Keychain(service: Keychain.Constant.Service.oauth),
+        userDefaults: UserDefaults = .standard
+    ) {
+        // DI
+        self.keychain = keyChain
+        self.userDefaults = userDefaults
+        
+        // 保存値の復元
+        self.accessToken = self.keychain[Keychain.Constant.Key.accessToken]
+        self.accessTokenExpiresAt = self.userDefaults.value(forKey: "TokenStore.accessTokenExpiresAt") as? Date
+    }
+}
+
