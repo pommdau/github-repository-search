9f7cc10d72f6d080cc8a4ff57f5fe648c59ee235
IKEH1024 <pondau1024@gmail.com>
2025-04-02 11:21:38 +0900

    wip

--- IKEHGitHubAPIDemo/IKEHGitHubAPIDemo/DataLayer/Store/SearchSuggestionStore.swift
+++ IKEHGitHubAPIDemo/IKEHGitHubAPIDemo/DataLayer/Store/SearchSuggestionStore.swift
@@ -10,12 +10,10 @@ import SwiftUI
 
 @MainActor
 protocol SearchSuggestionStoreProtocol: AnyObject {
+    static var shared: Self { get } // シングルトン
     var maxHistoryCount: Int { get } // 履歴の最大記憶数
     var recommendedSuggestions: [String] { get } // アプリ側で固定のレコメンド
-//    var shared: Self { get }
     var historySuggestions: [String] { get set } // 検索履歴
-    
-    
     func addHistory(_ keyword: String)
     func removeAllHistories()
 }
@@ -29,13 +27,6 @@ extension SearchSuggestionStoreProtocol {
     var recommendedSuggestions: [String] {
         ["SwiftUI", "Swift", "Python", "Apple", "Qiita"]
     }
-//    let shared: SearchSuggestionStore = .init()
-            
-//    var historySuggestions: [String] {
-//        didSet {
-//            userDefaults?.setCodableItem(historySuggestions, forKey: "suggestions-history")
-//        }
-//    }
     
     // MARK: - 履歴の追加/削除
     
@@ -58,6 +49,10 @@ extension SearchSuggestionStoreProtocol {
             historySuggestions.removeLast()
         }
     }
+    
+    func removeHistory(atOffsets offSets: IndexSet) {
+        historySuggestions.remove(atOffsets: offSets)
+    }
         
     func removeAllHistories() {
         historySuggestions.removeAll()
@@ -66,7 +61,9 @@ extension SearchSuggestionStoreProtocol {
 
 @MainActor
 @Observable
-class NewSearchSuggestionStore: SearchSuggestionStoreProtocol {
+final class SearchSuggestionStore: SearchSuggestionStoreProtocol {
+    
+    static let shared: SearchSuggestionStore = .init()
     
     var userDefaults: UserDefaults
     
@@ -80,79 +77,20 @@ class NewSearchSuggestionStore: SearchSuggestionStoreProtocol {
         self.userDefaults = userDefaults
         // 保存されている値の読込
         self.historySuggestions = userDefaults.codableItem(forKey: "suggestions-history") ?? []
+
     }
 }
 
 @MainActor
 @Observable
-final class NewSearchSuggestionStoreMock: SearchSuggestionStoreProtocol {
+final class SearchSuggestionStoreStub: SearchSuggestionStoreProtocol {
+    
+    static let shared: SearchSuggestionStoreStub = .init()
     
     let userDefaults: UserDefaults? = nil
     var historySuggestions: [String]
     
     init(historySuggestions: [String] = []) {
         self.historySuggestions = historySuggestions
-    }
-}
-
-// MARK: -
-
-@MainActor
-@Observable
-class SearchSuggestionStore {
-    
-    // MARK: - Property
-            
-    private static let maxHistoryCount = 5 // 履歴の最大記憶数
-    static let recommendedSuggestions = ["SwiftUI", "Swift", "Python", "Apple", "Qiita"] // 固定のオススメ
-    static let shared: SearchSuggestionStore = .init()
-    
-    var userDefaults: UserDefaults?
-    
-    var historySuggestions: [String] {
-        didSet {
-            userDefaults?.setCodableItem(historySuggestions, forKey: "suggestions-history")
-        }
-    }
-    
-    // MARK: - LifeCycle
-    
-    private init(userDefaults: UserDefaults? = .standard) {
-        self.userDefaults = userDefaults
-        // 保存されている値の読込
-        self.historySuggestions = userDefaults?.codableItem(forKey: "suggestions-history") ?? []
-    }
-}
-
-// MARK: - CRUD
-
-extension SearchSuggestionStore {
-    
-    // MARK: Create
-    
-    func addHistory(_ keyword: String) {
-        if keyword.isEmpty {
-            return
-        }
-        // 履歴にある場合は最新になるように再配置
-        if let index = historySuggestions.firstIndex(where: { $0 == keyword }) {
-            historySuggestions.remove(at: index)
-            historySuggestions.insert(keyword, at: 0)
-            return
-        }
-        
-        // 履歴になければ検索された語句を追加
-        historySuggestions.insert(keyword, at: 0)
-        
-        // 履歴の上限を超えた分を古い順に削除
-        while historySuggestions.count > Self.maxHistoryCount {
-            historySuggestions.removeLast()
-        }
-    }
-    
-    // MARK: Delete
-    
-    func removeAllHistories() {
-        historySuggestions.removeAll()
     }
 }
