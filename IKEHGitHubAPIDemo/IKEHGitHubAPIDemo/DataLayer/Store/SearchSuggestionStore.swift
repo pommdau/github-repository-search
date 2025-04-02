f6b91311344d9f754d94bd1693dc9d0211fb0519
IKEH1024 <pondau1024@gmail.com>
2025-04-02 10:46:00 +0900

    wip

--- IKEHGitHubAPIDemo/IKEHGitHubAPIDemo/DataLayer/Store/SearchSuggestionStore.swift
+++ IKEHGitHubAPIDemo/IKEHGitHubAPIDemo/DataLayer/Store/SearchSuggestionStore.swift
@@ -8,8 +8,94 @@
 
 import SwiftUI
 
-// MARK: -
+@MainActor
+protocol SearchSuggestionStoreProtocol: AnyObject {
+    var maxHistoryCount: Int { get } // 履歴の最大記憶数
+    var recommendedSuggestions: [String] { get } // アプリ側で固定のレコメンド
+//    var shared: Self { get }
+    var historySuggestions: [String] { get set } // 検索履歴
+    
+    
+    func addHistory(_ keyword: String)
+    func removeAllHistories()
+}
 
+extension SearchSuggestionStoreProtocol {
+    
+    var maxHistoryCount: Int {
+        5
+    }
+    
+    var recommendedSuggestions: [String] {
+        ["SwiftUI", "Swift", "Python", "Apple", "Qiita"]
+    }
+//    let shared: SearchSuggestionStore = .init()
+            
+//    var historySuggestions: [String] {
+//        didSet {
+//            userDefaults?.setCodableItem(historySuggestions, forKey: "suggestions-history")
+//        }
+//    }
+    
+    // MARK: - 履歴の追加/削除
+    
+    func addHistory(_ keyword: String) {
+        if keyword.isEmpty {
+            return
+        }
+        // 履歴にある場合は最新になるように再配置
+        if let index = historySuggestions.firstIndex(where: { $0 == keyword }) {
+            historySuggestions.remove(at: index)
+            historySuggestions.insert(keyword, at: 0)
+            return
+        }
+        
+        // 履歴になければ検索された語句を追加
+        historySuggestions.insert(keyword, at: 0)
+        
+        // 履歴の上限を超えた分を古い順に削除
+        while historySuggestions.count > maxHistoryCount {
+            historySuggestions.removeLast()
+        }
+    }
+        
+    func removeAllHistories() {
+        historySuggestions.removeAll()
+    }
+}
+
+@MainActor
+@Observable
+class NewSearchSuggestionStore: SearchSuggestionStoreProtocol {
+    
+    var userDefaults: UserDefaults
+    
+    var historySuggestions: [String] {
+        didSet {
+            userDefaults.setCodableItem(historySuggestions, forKey: "suggestions-history")
+        }
+    }
+    
+    init(userDefaults: UserDefaults = .standard) {
+        self.userDefaults = userDefaults
+        // 保存されている値の読込
+        self.historySuggestions = userDefaults.codableItem(forKey: "suggestions-history") ?? []
+    }
+}
+
+@MainActor
+@Observable
+final class NewSearchSuggestionStoreMock: SearchSuggestionStoreProtocol {
+    
+    let userDefaults: UserDefaults? = nil
+    var historySuggestions: [String]
+    
+    init(historySuggestions: [String] = []) {
+        self.historySuggestions = historySuggestions
+    }
+}
+
+// MARK: -
 
 @MainActor
 @Observable
@@ -19,20 +105,22 @@ class SearchSuggestionStore {
             
     private static let maxHistoryCount = 5 // 履歴の最大記憶数
     static let recommendedSuggestions = ["SwiftUI", "Swift", "Python", "Apple", "Qiita"] // 固定のオススメ
-    
     static let shared: SearchSuggestionStore = .init()
+    
+    var userDefaults: UserDefaults?
     
     var historySuggestions: [String] {
         didSet {
-            UserDefaults.standard.setCodableItem(historySuggestions, forKey: "suggestions-history")
+            userDefaults?.setCodableItem(historySuggestions, forKey: "suggestions-history")
         }
     }
     
     // MARK: - LifeCycle
     
-    private init() {
+    private init(userDefaults: UserDefaults? = .standard) {
+        self.userDefaults = userDefaults
         // 保存されている値の読込
-        self.historySuggestions = UserDefaults.standard.codableItem(forKey: "suggestions-history") ?? []
+        self.historySuggestions = userDefaults?.codableItem(forKey: "suggestions-history") ?? []
     }
 }
 
