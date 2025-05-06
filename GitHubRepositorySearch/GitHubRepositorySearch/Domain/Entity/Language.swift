844c5d9a91d774da8472edddeedb8e77f65fbec9
IKEH1024 <pondau1024@gmail.com>
2025-05-06 22:26:12 +0900

    feat: 言語のモデルクラスの実装

---
+++ GitHubRepositorySearch/GitHubRepositorySearch/Domain/Entity/Language.swift
@@ -0,0 +1,34 @@
+//
+//  Language.swift
+//  IKEHGitHubAPIDemo
+//
+//  Created by HIROKI IKEUCHI on 2025/02/14.
+//
+
+import SwiftUI
+import struct IKEHGitHubAPIClient.SwiftID
+
+struct Language: Identifiable, Equatable, Sendable {
+    let id: SwiftID<Self>
+    var name: String
+    var color: Color
+
+    /// Initializer
+    /// - Parameters:
+    ///   - name: Language name
+    ///   - hex: e.g.  "#00cafe"
+    init(name: String, hex: String) {
+        self.id = .init(rawValue: UUID().uuidString)
+        self.name = name
+        self.color = Color(hex: hex.replacingOccurrences(of: "#", with: ""))
+    }
+}
+
+// MARK: - Mock
+
+extension Language {
+    enum Mock {
+        static let swift: Language = .init(name: "Swift", hex: "#F05138")
+        static let python: Language = .init(name: "Python", hex: "#3572A5")
+    }
+}
